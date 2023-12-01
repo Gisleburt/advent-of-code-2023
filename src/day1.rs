0016108f184855cdd79fb5971981302b3a133f76
Daniel Mason <daniel.mason@beamery.com>
2023-12-01 10:53:09 +0000

    day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,49 @@
-pub fn day1part1(input: String) -> String {
-    todo!()
+use anyhow::Result;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Day1Error {
+    #[error("Number not found in string")]
+    NoNumberFound,
+}
+
+fn first_number(input: &str) -> Result<usize> {
+    input
+        .chars()
+        .find(|c| c.is_numeric())
+        .and_then(|c| (c as usize).checked_sub(48))
+        .ok_or_else(|| Day1Error::NoNumberFound.into())
+        .into()
+}
+
+fn last_number(input: &str) -> Result<usize> {
+    input
+        .chars()
+        .rev()
+        .find(|c| c.is_numeric())
+        .and_then(|c| (c as usize).checked_sub(48))
+        .ok_or_else(|| Day1Error::NoNumberFound.into())
+}
+
+pub fn day1part1(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| (first_number(line).unwrap(), last_number(line).unwrap()))
+        .map(|(a, b)| (a * 10) + b)
+        .sum::<usize>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_day1part1() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(day1part1(input), "142");
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     let input = read_to_string(opt.input).expect("input not found");
 
     let result = match (opt.day, opt.part) {
-        (1, 1) => day1::day1part1(input),
+        (1, 1) => day1::day1part1(&input),
         _ => {
             eprintln!("Day {} part {} not found", opt.day, opt.part);
             exit(1);
